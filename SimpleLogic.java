cba1b48127a25dab3ef0afa827547c45f70907a7
ajscodes <ayushmaradia@gmail.com>
2024-06-21 21:59:24 +0530

    Length of args

---
+++ SimpleLogic.java
@@ -0,0 +1,7 @@
+public class SimpleLogic{
+    public static void main(String[] args) {
+        System.out.print(args.length);
+        //it's shows length of given command line arguments
+        //Command line: No ---> Answer: 0 
+    }
+}
