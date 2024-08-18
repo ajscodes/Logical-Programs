a849fb40705d48e80bd785832ba43c30b8ca7ca2
ajscodes <ayushmaradia@gmail.com>
2024-08-18 21:24:30 +0530

    Simple Logic

---
+++ FunWithJs.js
@@ -0,0 +1,12 @@
+//Question: what's output of console.log(typeof NaN) in javascript?
+console.log(typeof NaN); //number
+//Logic: 
+//NaN = Not a Number but type of it is number because it is technically a numeric value representing an invalid number
+//or undefined mathematical operation.
+
+
+//Question: What will output of console.log('true + false') & console.log(true + false) ?
+console.log('true + false'); //true + false
+console.log(true + false); //1
+//Logic:
+//('true + false') become string and print as it is but for (true + false), true = 1 and false = 0 --> 1+0 = 1
