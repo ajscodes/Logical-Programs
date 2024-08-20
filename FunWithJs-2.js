02671c12f254415c20d416cb9f70bd439a5c318f
ajscodes <ayushmaradia@gmail.com>
2024-08-20 22:49:46 +0530

    + and -

---
+++ FunWithJs-2.js
@@ -0,0 +1,6 @@
+console.log(20+"1") //201
+console.log(20-"1") //19
+
+// + (Addition/Concatenation): When one of the operands is a string, JavaScript defaults to string concatenation.
+// - (Subtraction): JavaScript interprets the operation as numeric, so it converts both operands to numbers if necessary.
+// This behavior is a result of JavaScript's loose typing and automatic type coercion, which can sometimes lead to unexpected results.
